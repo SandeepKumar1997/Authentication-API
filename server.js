496be93f91b78d5946e6945a9bc5923e9c89e834
Hari <sandhari@deloitte.com>
2022-07-16 18:05:39 +0530

    API for Authentication V1

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+const goals = require("./routes/testroute");
+const users = require("./routes/userRoutes");
+const makeConnectionMongo = require("./config/mongo");
+
+dotenv.config({ path: "./config/config.env" });
+makeConnectionMongo();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.use("/api/goals", goals);
+app.use("/api/users", users);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
